0e8d1113e8ece986b9bd0c86ba2933270145e851
Vineet Sharma <vsharma@objectsharp.com>
2021-09-19 09:41:42 -0400

    changing to dog facts because cat facts api is not working

--- .github/actions/cat-facts/src/main.py
+++ .github/actions/cat-facts/src/main.py
@@ -3,7 +3,8 @@ import random
 import sys
 
 # Make an HTTP GET request to the cat-fact API
-cat_url = "https://cat-fact.herokuapp.com/facts"
+#cat_url = "https://cat-fact.herokuapp.com/facts"
+cat_url = "https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all"
 r = requests.get(cat_url)
 r_obj_list = r.json()
 
